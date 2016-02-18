ba5f04ce78e1df412ef6ccb82ac6883e343cbd26
Alexey Belov <alexeybelov@bk.ru>
2016-02-19 01:10:18 +0300

    equation

--- src/Equation.java
+++ src/Equation.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 public class Equation {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 2;
-        int c = 2;
-        int x = 0;
-//        System.out.println((a * (x * x) )+ x * b + c = 0));
+        boolean firstCoefficient = true;
+        int discriminant;
+        int result;
+        int result2;
+        int a = 0;
+        while (firstCoefficient) {
+            Scanner scA = new Scanner(System.in);
+            System.out.print("Введите целое число: ");
+            a = scA.nextInt();
+            if (a == 0) {
+                System.out.println("Повторите ввод");
+
+            }else firstCoefficient = false;
+        }
+        Scanner scB = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int b = scB.nextInt();
+        Scanner sC = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int c = sC.nextInt();
+
+        discriminant = b * b - 4 * a * c;
+        System.out.println(discriminant);
+        if (discriminant < 0) {
+            System.out.println("Нет действительных решений уравнения");
+        } else if (discriminant == 0) {
+            result = -1 * b / (2 * a);
+            System.out.println(result);
+        } else if (discriminant > 0) {
+            result = (int) ((-1 * b + Math.sqrt(discriminant)) / 2 * a);
+            result2 = (int) ((-1 * b - Math.sqrt(discriminant)) / 2 * a);
+            System.out.println(result + " " + result2);
+        }
     }
 }
