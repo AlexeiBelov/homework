4656e97d007eb17f4c2bb034d68287671f4d3f75
Alexey Belov <alexeybelov@bk.ru>
2016-02-18 16:44:27 +0300

    CalculatingTheSquareRoo

---
+++ src/Equation.java
@@ -0,0 +1,9 @@
+public class Equation {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = 2;
+        int x = 0;
+//        System.out.println((a * (x * x) )+ x * b + c = 0));
+    }
+}
